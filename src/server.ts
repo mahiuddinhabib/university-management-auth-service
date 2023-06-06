afdbf577328839e45863f29bcb61de534546b20a
mahiuddinhabib <mh.habib137@gmail.com>
2023-06-07 01:03:22 +0600

    improved linting setup

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ import config from './config'
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('DB is connected successfully')
+    // console.log('DB is connected successfully')
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      // console.log(`Example app listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed to connect DB')
+    // console.log('Failed to connect DB')
   }
 }
 
